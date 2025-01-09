e4e7fb1931dfd81589eed868c52fec3aa99b9f40
yourong <yourongx@uw.edu>
2025-01-08 17:17:00 -0800

    Updated OLED project with PlatformIO configuration and example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-#include <Arduino.h>  // 
-
-// 
+#include <Arduino.h>   
+ 
 void setup() {
-  Serial.begin(115200);  // 
+  Serial.begin(115200);  
 }
 
-// 
 void loop() {
-  Serial.println("Hello XIAO!");  // 
-  delay(2000);  //
+  Serial.println("Hello XIAO!"); 
+  delay(2000); 
 }
